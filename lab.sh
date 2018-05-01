bf368c54c2b667a8c8ece425c74e1d21b1daf44c
KnoeHow866 <accidental001@gmail.com>
2018-05-01 08:23:11 +0800

    doc(lab.sh)

--- lab.sh
+++ lab.sh
@@ -13,8 +13,11 @@ mkdir app/raw-data/$model-training
 batch=(1 2 4 8 10 16 20 25 32)
 
 for ((i=0; i<${#batch[@]}; i++)); do
-    # python tf_cnn_benchmarks.py --num_gpus=1 --model=$model --variable_update=parameter_server  --batch_size=${batch[$i]} > report/app/raw-data/$model-training/gpu-$model-b${batch[$i]}.txt
-    python tf_cnn_benchmarks.py --device=gpu --model=$model --variable_update=parameter_server  --batch_size=${batch[$i]} > app/raw-data/$model-training/gpu-$model-b${batch[$i]}.txt
+    # cpu 
+    python tf_cnn_benchmarks.py --device=cpu --data_format=NHWC --model=$model --variable_update=parameter_server  --batch_size=${batch[$i]} > report/app/raw-data/$model-training/gpu-$model-b${batch[$i]}.txt
+    
+    # GPU
+    python tf_cnn_benchmarks.py --num_gpus=1 --model=$model --variable_update=parameter_server  --batch_size=${batch[$i]} > app/raw-data/$model-training/gpu-$model-b${batch[$i]}.txt
     wait
     python tf_cnn_benchmarks.py --num_gpus=1 --model=$model --variable_update=parameter_server --forward_only=True --batch_size=${batch[$i]} > app/raw-data/$model/gpu-$model-b${batch[$i]}.txt
     wait
